087b29e620f2c023da3ceb558019e6186999085b
cyngularsecurity <tomerga@cyngularsecurity.com>
2024-11-13 11:55:15 +0200

    cross envs adjustment

--- UpdateSecret.sh
+++ UpdateSecret.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SECRET_NAME="ip_whitelist"
-KEY_NAME=$1
+KEY_NAME=$2
 
-CURRENT_SECRET=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" --query 'SecretString' --output text --region us-west-2)
+CURRENT_SECRET=$(aws secretsmanager get-secret-value --secret-id "$SECRET_NAME" --query 'SecretString' --output text --profile $1)
 
 
 NEW_VALUE=$(curl -s https://checkip.amazonaws.com/)/32
@@ -11,7 +11,7 @@ NEW_VALUE=$(curl -s https://checkip.amazonaws.com/)/32
 
 UPDATED_SECRET=$(echo "$CURRENT_SECRET" | jq --arg key "$KEY_NAME" --arg value "$NEW_VALUE" '.[$key] = $value')
 
-aws secretsmanager update-secret --secret-id "$SECRET_NAME" --secret-string "$UPDATED_SECRET" --region us-west-2
+aws secretsmanager update-secret --secret-id "$SECRET_NAME" --secret-string "$UPDATED_SECRET" --profile $1
 
 if [ "$?" -eq 0 ]; then
     echo "Successfully updated the secret $SECRET_NAME with the new value for key $KEY_NAME."
